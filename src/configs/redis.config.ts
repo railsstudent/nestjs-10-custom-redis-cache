782ee310b4922b1e4ace0216822a3b4a22f8b20c
Connie Leung <connie.leung@diginex.com>
2023-11-24 17:23:27 +0800

    fix: calculation error.  10 minutes has 600 seconds

--- src/configs/redis.config.ts
+++ src/configs/redis.config.ts
@@ -3,5 +3,5 @@ import { registerAs } from '@nestjs/config';
 export default registerAs('redis', () => ({
   host: process.env.REDIS_HOST || 'localhost',
   port: parseInt(process.env.REDIS_PORT, 10) || 6379,
-  ttl: parseInt(process.env.REDIS_TTL, 10) || 60,
+  ttl: parseInt(process.env.REDIS_TTL, 10) || 600,
 }));
